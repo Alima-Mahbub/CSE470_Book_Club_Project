c6968beae83d3f0aa51ad2ca632963ccfaa55930
Alima Mahbub github <rashidabilkis01@gmail.com>
2024-07-06 02:51:50 +0600

    Resolved merge conflicts

---
+++ frontend/src/pages/AboutUs.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles.css';
+
+const AboutUs = () => {
+  return (
+    <div className="text-container">
+      <h1>About Us</h1>
+      <p>This is the About Us page. You can edit this text later.</p>
+    </div>
+  );
+};
+
+export default AboutUs;
